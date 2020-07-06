107c368bc7cf0302fee7edae39aea3649cb9f2fa
prapra@hotmail.de <prapra@hotmail.de>
2020-07-06 17:12:01 +0200

    add correct bpmn file - clean from references

---
+++ businessLogic/src/main/java/com/accarda/panicPoc/application/rest/service/CamundaRestClientConstants.java
@@ -0,0 +1,14 @@
+package com.accarda.panicPoc.application.rest.service;
+
+public final class CamundaRestClientConstants {
+
+    private CamundaRestClientConstants() {
+    }
+
+    // http://localhost:8080/engine-rest/
+
+    private static  final   String  PROTOCOL            =   "http";
+    private static  final   String  HOST                =   "localhost";
+    private static  final   String  PORT                =   "8080";
+    private static  final   String  CAMUNDA_REST_API    =   "engine-rest";
+}
